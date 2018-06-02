aaa6885d15718f5ed2fcc6d73437c33ab909b385
podiumdesu <wwhmomo@gmail.com>
2018-06-03 02:31:30 +0800

    ADD: several functions to process corresponding data

--- server/rsshub/getData.js
+++ server/rsshub/getData.js
@@ -5,7 +5,7 @@ const a = {
       for (let i = 0; i < data.items.length; i++) {
         result.push(
           {
-            type: 'bilibili',
+            type: 'b站订阅番剧',
             author: 'none',
             lastUpdate: data.items[i].date_published,
             title: data.title,
@@ -19,7 +19,141 @@ const a = {
       return result
     },
     scriptWeb: 'https://rsshub.app/bilibili/bangumi/'
-},
+  },
+  // b站用户投稿
+  upSubmit: {
+    filter: function(data) {
+      const result = []
+      for (let i = 0; i < data.items.length; i++) {
+        result.push(
+          {
+            type: 'Up主投稿',
+            author: 'none',
+            lastUpdate: data.items[i].date_published,
+            title: data.items[i].title,
+            isNew: true,
+            content: data.items[i].summary,
+            images: [],
+            source: this.items[i].url
+          }
+        )
+      }
+      return result
+    },
+    scriptWeb: 'https://rsshub.app/bilibili/user/video/'
+  },
+  // b站动态
+  upDynamic: {
+    filter: function(data) {
+      const result = []
+      for (let i = 0; i < data.items.length; i++) {
+        result.push(
+          {
+            type: 'Up主动态',
+            author: 'none',
+            lastUpdate: data.items[i].date_published,
+            title: data.items[i].title,
+            isNew: true,
+            content: data.items[i].summary,
+            images: [],
+            source: this.items[i].url
+          }
+        )
+      }
+      return result
+    },
+    scriptWeb: 'https://rsshub.app/bilibili/user/dynamic/'
+  },
+  // 微博关注
+  weiboUser: {
+    filter: function(data) {
+      const result = []
+      for (let i = 0; i < data.items.length; i++) {
+        result.push(
+          {
+            type: '微博关注',
+            author: 'none',
+            lastUpdate: data.items[i].date_published,
+            title: data.items[i].title,
+            isNew: true,
+            content: data.items[i].summary,
+            images: [],
+            source: this.items[i].url
+          }
+        )
+      }
+      return result
+    },
+    scriptWeb: 'https://rsshub.app/weibo/user/'
+  },
+  // 掘金
+  juejin: {
+    filter: function(data) {
+      const result = []
+      for (let i = 0; i < data.items.length; i++) {
+        result.push(
+          {
+            type: data.title,
+            author: 'none',
+            lastUpdate: data.items[i].date_published,
+            title: data.items[i].title,
+            isNew: true,
+            content: data.items[i].summary,
+            images: [],
+            source: this.items[i].url
+          }
+        )
+      }
+      return result
+    },
+    scriptWeb: 'https://rsshub.app/juejin/category/'
+  },
+  //简书作者
+  jianshuWriter: {
+    filter: function(data) {
+      const result = []
+      for (let i = 0; i < data.items.length; i++) {
+        result.push(
+          {
+            type: '简书关注',
+            author: 'none',
+            lastUpdate: data.items[i].date_published,
+            title: data.items[i].title,
+            isNew: true,
+            content: data.items[i].summary,
+            images: data.items[i].image,
+            source: this.items[i].url
+          }
+        )
+      }
+      return result
+    },
+    scriptWeb: 'https://rsshub.app/jianshu/user/'
+  },
+  // 知乎用户
+  jianshuWriter: {
+    filter: function(data) {
+      const result = []
+      for (let i = 0; i < data.items.length; i++) {
+        result.push(
+          {
+            type: '简书首页',
+            author: 'none',
+            lastUpdate: data.items[i].date_published,
+            title: data.items[i].title,
+            isNew: true,
+            content: data.items[i].summary,
+            images: data.items[i].image,
+            source: this.items[i].url
+          }
+        )
+      }
+      return result
+    },
+    scriptWeb: 'https://rsshub.app/zhihu/people/activities/'
+  },
+
+
 
 }
 
