69b88573bf1d3cba27f99cd57f7fdba0c2fecfbb
podiumdesu <wwhmomo@gmail.com>
2018-06-03 11:58:57 +0800

    DATA: add test data

---
+++ server/rss/testfile.js
@@ -0,0 +1,78 @@
+{
+  "type": "bangumiUpdate",
+  "uid": "21680"
+}
+
+{
+  "type": "upSubmit",
+  "uid": "4377887"
+}
+
+// 王老菊
+{
+  "type": "upDynamic",
+  "uid": "423895"
+}
+
+// 微博用户
+// LGD电子竞技俱乐部
+{
+  "type": "weiboUser",
+  "uid": "2157471171"
+}
+
+// 【good】掘金前端
+{
+  "type": "juejin",
+  "uid": "frontend"
+}
+
+// 简书作者
+{
+  "type": "jianshuWriter",
+  "uid": "92eb338437ee"
+}
+
+// 知乎作者
+// vzch 【good】
+{
+  "type": "zhihuWriter",
+  "uid": "vzch"
+}
+
+// p站排行
+// week
+
+{
+  "type": "pixivRank",
+  "uid": "week"
+}
+
+// 开发者头条
+// 【good】today
+{
+  "type": "developerToutiao",
+  "uid": "today"
+}
+
+// 今日头条分类
+// 【good】ai
+{
+  "type": "toutiaoKeyword",
+  "uid": "ai"
+}
+
+// 斗鱼直播
+// douyuStream
+// good
+{
+  "type": "douyuStream",
+  "uid": "97376"
+}
+
+// v2ex最新
+// 【good】 latest
+{
+  "type": "v2exPopular",
+  "uid": "latest"
+}
